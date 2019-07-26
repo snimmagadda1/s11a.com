2bd788d011540fc75fe11aed1ff60e3384b53b3b
snimmag6 <sai.nimmagadda@optum.com>
2019-07-26 11:25:30 -0400

    Add scss primitive theme

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.css";
+import '../styles/main.scss'
 
 export default class MainLayout extends React.Component {
   render() {
