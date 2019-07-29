1e682be88233f9e4f7f1e9ee8d45bdd0429f50ba
snimmag6 <sai.nimmagadda@optum.com>
2019-07-29 15:32:32 -0400

    Make the nav scroll H.O.T

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,33 @@ import React, { Component } from "react";
 import { Link } from "gatsby";
 
 export default class Navbar extends Component {
+
+  state = {
+    scrolled: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.navOnScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.navOnScroll)
+  }
+
+  navOnScroll = () => {
+    if (window.scrollY > 20) {
+      this.setState({ scrolled: true })
+    } else {
+      this.setState({ scrolled: false })
+    }
+  }
+
   render() {
+    const { scrolled } = this.state
     const { menuLinks } = this.props
 
     return (
-      <div className="nav">
+      <div className={scrolled ? 'nav scroll' : 'nav'}>
         <div className="nav-container">
           <div className="me">Sai Nimmagadda</div>
           <div className="links">
