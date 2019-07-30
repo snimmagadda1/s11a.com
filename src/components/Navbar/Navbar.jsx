20a947f86064bff8ecf175b2f9701682badd7713
snimmag6 <sai.nimmagadda@optum.com>
2019-07-29 21:41:18 -0400

    style search page, add emoji to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
+import face from "../../images/face.png";
 
 export default class Navbar extends Component {
-
   state = {
-    scrolled: false,
-  }
+    scrolled: false
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.navOnScroll)
+    window.addEventListener("scroll", this.navOnScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.navOnScroll)
+    window.removeEventListener("scroll", this.navOnScroll);
   }
 
   navOnScroll = () => {
     if (window.scrollY > 20) {
-      this.setState({ scrolled: true })
+      this.setState({ scrolled: true });
     } else {
-      this.setState({ scrolled: false })
+      this.setState({ scrolled: false });
     }
-  }
+  };
 
   render() {
-    const { scrolled } = this.state
-    const { menuLinks } = this.props
+    const { scrolled } = this.state;
+    const { menuLinks } = this.props;
 
     return (
-      <div className={scrolled ? 'nav scroll' : 'nav'}>
+      <div className={scrolled ? "nav scroll" : "nav"}>
         <div className="nav-container">
-          <div className="me"><Link key="sai" to="/" activeClassName="active">Sai Nimmagadda</Link></div>
+          <div className="me">
+            <Link key="sai" to="/" activeClassName="active">
+              <img src={face} className="favicon" alt="Face" />
+              <span>Sai Nimmagadda</span>
+            </Link>
+          </div>
           <div className="links">
             {menuLinks.map(link => (
               <Link key={link.name} to={link.link} activeClassName="active">
