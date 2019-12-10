1618ecb66871329a6546bb5ce2339eca4070ae51
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-09 21:55:51 -0500

    Begin footer

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -13,8 +13,20 @@ export default class MainLayout extends React.Component {
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
-        <Navbar menuLinks={config.menuLinks}/>
+        <Navbar menuLinks={config.menuLinks} />
         <main id="main-content">{children}</main>
+        <div className="container footer">
+          "testing"
+          <div>
+            <a href="https://github.com/snimmagadda1" title="Contribute on GitHub">
+              {/* <img src="/static/github-d160ace5798527574789e89ec034e8f7.png" target="_blank" rel="noopener noreferrer" class="footer-img" alt="GitHub">
+              </img> */}
+              Github
+            </a>
+            <a href="https://www.netlify.com/" title="Hosted by Netlify">Netlify</a>
+            <a href="https://www.gatsbyjs.org/" title="Built with Gatsby">Gatsby</a>
+          </div>
+        </div>
       </div>
     );
   }
