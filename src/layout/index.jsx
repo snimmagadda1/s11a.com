6875cbfc17fa71a55ba78f149900ff6555f2db16
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-14 16:29:43 -0500

    Add footer

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,6 +4,7 @@ import config from "../../data/SiteConfig";
 import "./index.css";
 import '../styles/main.scss'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer';
 
 export default class MainLayout extends React.Component {
   render() {
@@ -15,18 +16,8 @@ export default class MainLayout extends React.Component {
         </Helmet>
         <Navbar menuLinks={config.menuLinks} />
         <main id="main-content">{children}</main>
-        <div className="container footer">
-          "testing"
-          <div>
-            <a href="https://github.com/snimmagadda1" title="Contribute on GitHub">
-              {/* <img src="/static/github-d160ace5798527574789e89ec034e8f7.png" target="_blank" rel="noopener noreferrer" class="footer-img" alt="GitHub">
-              </img> */}
-              Github
-            </a>
-            <a href="https://www.netlify.com/" title="Hosted by Netlify">Netlify</a>
-            <a href="https://www.gatsbyjs.org/" title="Built with Gatsby">Gatsby</a>
-          </div>
-        </div>
+        <Footer></Footer>
+
       </div>
     );
   }
