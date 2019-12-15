cfc67255f80606a32408529e154cdf3819447554
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-15 18:25:57 -0500

    Begin projects list component

---
+++ data/projects.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        icon: '🧄',
+        title: 'Garlic-bot',
+        path: 'https://twitter.com/garlichub',
+        source: 'https://github.com/snimmagadda1/garlic-bot',
+        description: 'A serverless bot that RTs garlic when it feels like it',
+    },
+    {
+        icon: '🚧',
+        title: 'azure-terraform-generator',
+        path: '',
+        source: 'https://github.com/snimmagadda1/azure-terraform-generator',
+        description: 'CLI tool to query resources within a Microsoft Azure deployment and generate terraform definitions for existing objects',
+    },
+    {
+        icon: '🐥',
+        title: 'FPGA-Flappy-Bird',
+        path: '',
+        source: 'https://github.com/snimmagadda1/FPGA-Flappy-Bird',
+        description: 'Old school project. Run custom build flappy bird on custom build processor (verilog)',
+    },
+]
