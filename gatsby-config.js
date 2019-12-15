c4e709dd4983938c640989fc8cb80b7c75443303
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-15 11:34:44 -0500

    Fix file not found

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,19 +86,7 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+        display: "minimal-ui"
       }
     },
     "gatsby-plugin-offline",
