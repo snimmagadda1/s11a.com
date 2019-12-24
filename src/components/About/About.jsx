95a5483d7e4950d11e9db2300a1e592a53d9caf1
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-24 13:22:13 -0600

    Update about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
-
+import mePic from '../../images/me.jpg'
+import leetCode from '../../../static/logos/leetcode.png'
+import udemy from '../../../static/logos/udemy.png'
 class About extends Component {
+
+  navigateToSomething = (url) => {
+    window.open(url, '_blank')
+  }
+
   render() {
     return (
       <div className="about">
         <h1>About me</h1>
-        <div className="content">
+        <div className="text-content">
           <p>Hey there 👋! My name is Sai Nimmagadda. I currently work as a full stack software engineer and have a BSE in electrical and computer engineering and biomedical engineering from Duke.</p>
           <p>I'm hoping to grow this site in parallel with my exploration of new tech and open-source software.
             I also created this site to keep myself on track: I love the excitement that comes with contributing to the web and wanted a way combine my learning with contributing.
           </p>
-          <p>I believe the best way to learn is through collaboration with others, so I'll be posting anything and everything I come across in the web that I think might be useful or interesting. 
+          <p>I believe the best way to learn is through collaboration with others, so I'll be posting anything and everything I come across in the web that I think might be useful or interesting.
             Hopefully visitors will find the knowledge/rants here useful.</p>
+        </div>
+        <div className="me-content">
+          <div className="currently">
+            <h3>What I'm up to: </h3>
+            <button className="muted-button"
+              onClick={() => this.navigateToSomething("https://github.com/snimmagadda1/coding-challenges")}>
+              Tryna remember DS & algos
+              <img
+                src={leetCode}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-pic"
+                alt="LeetCode"
+              />
+            </button>
+            <button className="muted-button"
+              onClick={() => this.navigateToSomething("https://www.udemy.com/course/go-programming-language/")}>
+              Learning Golang
+              <img
+                src={udemy}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-pic"
+                alt="LeetCode"
+              />
+            </button>
+          </div>
+          <img
+            src={mePic}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="me-pic"
+            alt="Me picture"
+          />
         </div>
       </div>
     );
