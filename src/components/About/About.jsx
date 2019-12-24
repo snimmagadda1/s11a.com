5535021c1e8c3be471702cf5267a0f12deff2ce8
Sai Nimmagadda <saiguy@me.com>
2019-12-24 15:20:53 -0600

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,7 +15,7 @@ class About extends Component {
         <div className="text-content">
           <p>Hey there 👋! My name is Sai Nimmagadda. I currently work as a full stack software engineer and have a BSE in electrical and computer engineering and biomedical engineering from Duke.</p>
           <p>I'm hoping to grow this site in parallel with my exploration of new tech and open-source software.
-            I also created this site to keep myself on track: I love the excitement that comes with contributing to the web and wanted a way combine my learning with contributing.
+            I also created this site to keep myself on track: I love the excitement that comes with contributing to the web and wanted a way to combine my learning with contributing.
           </p>
           <p>I believe the best way to learn is through collaboration with others, so I'll be posting anything and everything I come across in the web that I think might be useful or interesting.
             Hopefully visitors will find the knowledge/rants here useful.</p>
