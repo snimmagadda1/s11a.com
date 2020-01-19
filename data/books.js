03ea68bd06d922405a2a6e5bfc863557fde8f8e8
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2020-01-19 17:34:05 -0500

    Add books list data

---
+++ data/books.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        title: 'Web Scalability for Startup Engineers',
+        author: 'Artur Ejsmont',
+        source: 'https://www.amazon.com/Scalability-Startup-Engineers-Artur-Ejsmont/dp/0071843655/ref=sr_1_fkmr0_1?keywords=Web+Scalability+for+Startup+Engineers+1st+Edition&qid=1579473174&s=books&sr=1-1-fkmr0',
+    },
+    {
+        title: 'Building Microservices: Designing Fine-Grained Systems ',
+        author: 'Sam Newman',
+        source: 'https://www.amazon.com/Building-Microservices-Designing-Fine-Grained-Systems-ebook/dp/B00T3N7XB4/ref=sr_1_fkmr0_1?keywords=Building+Microservices%3A+Designing+Fine-Grained+Systems+1st+Edition%2C+Kindle+Edition&qid=1579473208&s=digital-text&sr=1-1-fkmr0',
+    },
+]
