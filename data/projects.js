8f0b5996bb0c9547cdf58ba5aa4353e1ce7ca867
snimmagadda1 <saiguy@me.com>
2024-09-28 10:35:08 -0400

    project b/c fun

--- data/projects.js
+++ data/projects.js
@@ -1,32 +1,40 @@
 export default [
-    {
-        icon: "🧄",
-        title: "Garlic-bot",
-        path: "https://twitter.com/garlichub",
-        source: "https://github.com/snimmagadda1/garlic-bot",
-        description: "A serverless bot that RTs garlic when it feels like it",
-    },
-    {
-        icon: "🐙",
-        title: "Stack Exchange GraphQL Server",
-        path: "https://stacke-graphql.azurewebsites.net",
-        source: "https://github.com/snimmagadda1/stack-exchange-graphql-server",
-        description: "A GraphQL endpoint for Stack Exchange data powered by Go",
-    },
-    {
-        icon: "🚧",
-        title: "azure-terraform-generator",
-        path: "",
-        source: "https://github.com/snimmagadda1/azure-terraform-generator",
-        description:
-            "CLI tool to query resources and generate terraform definitions for existing objects",
-    },
-    {
-        icon: "🐥",
-        title: "FPGA-Flappy-Bird",
-        path: "",
-        source: "https://github.com/snimmagadda1/FPGA-Flappy-Bird",
-        description:
-            "Old school project. Run custom build flappy bird on custom build processor (verilog)",
-    },
+  {
+    icon: "🧄",
+    title: "Garlic-bot",
+    path: "https://twitter.com/garlichub",
+    source: "https://github.com/snimmagadda1/garlic-bot",
+    description: "A serverless bot that RTs garlic when it feels like it",
+  },
+  {
+    icon: "🐙",
+    title: "Stack Exchange GraphQL Server",
+    path: "https://stacke-graphql.azurewebsites.net",
+    source: "https://github.com/snimmagadda1/stack-exchange-graphql-server",
+    description: "A GraphQL endpoint for Stack Exchange data powered by Go",
+  },
+  {
+    icon: "🚧",
+    title: "azure-terraform-generator",
+    path: "",
+    source: "https://github.com/snimmagadda1/azure-terraform-generator",
+    description:
+      "CLI tool to query resources and generate terraform definitions for existing objects",
+  },
+  {
+    icon: "🐥",
+    title: "FPGA-Flappy-Bird",
+    path: "",
+    source: "https://github.com/snimmagadda1/FPGA-Flappy-Bird",
+    description:
+      "Old school project. Run custom build flappy bird on custom build processor (verilog)",
+  },
+  {
+    icon: "☕",
+    title: "(caf)FEINED",
+    path: "https://todo.s11a.com/home",
+    source: "https://github.com/snimmagadda1/feined-todo",
+    description:
+      "A minimalist, calendar-centric todo app... b/c everyone has to have one of these",
+  },
 ];
